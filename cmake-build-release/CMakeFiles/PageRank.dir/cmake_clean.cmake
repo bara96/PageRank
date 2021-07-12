7646f1297eb9d61c55f308b8adfb95d31c45627c
Matteo Baratella <matteo.baratella96@mail.com>
2021-07-12 14:30:16 +0200

    Init

---
+++ cmake-build-release/CMakeFiles/PageRank.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PageRank.dir/main.cpp.obj"
+  "PageRank.exe"
+  "PageRank.exe.manifest"
+  "PageRank.lib"
+  "PageRank.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PageRank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
